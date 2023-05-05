477519e8e8c32fa08556610f187d585b638456cc
0xOsiris <djosiris@proton.me>
2023-05-05 08:49:21 -0500

    Removed unnecessary sload from _updatePosition

--- contracts/UniswapV3Pool.sol
+++ contracts/UniswapV3Pool.sol
@@ -376,7 +376,7 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
     /// @param tickLower the lower tick of the position's tick range
     /// @param tickUpper the upper tick of the position's tick range
     /// @param tick the current tick, passed to avoid sloads
-    function _updatePosition(
+    function (
         address owner,
         int24 tickLower,
         int24 tickUpper,
@@ -397,7 +397,7 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
                 observations.observeSingle(
                     time,
                     0,
-                    slot0.tick,
+                    ,
                     slot0.observationIndex,
                     liquidity,
                     slot0.observationCardinality
